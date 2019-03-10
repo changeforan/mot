d2ddf75675413186433b2fb2576bc543a08239a4
Sheng@MateBook13 <changeforan@gmail.com>
2019-03-11 00:08:16 +0800

    dev

--- helper/kalman_filter.py
+++ helper/kalman_filter.py
@@ -1,13 +1,3 @@
-'''
-    File name         : kalman_filter.py
-    File Description  : Kalman Filter Algorithm Implementation
-    Author            : Srini Ananthakrishnan
-    Date created      : 07/14/2017
-    Date last modified: 07/16/2017
-    Python Version    : 2.7
-'''
-
-# Import python libraries
 import numpy as np
 
 
@@ -26,25 +16,38 @@ class KalmanFilter(object):
         Return:
             None
         """
-        self.dt = 0.005  # delta time
+        self.dt = 0.05   # delta time
 
-        self.A = np.array([[1, 0], [0, 1]])  # matrix in observation equations
-        self.u = np.zeros((2, 1))  # previous state vector
+        self.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])# matrix in observation equations, measurement function A ==> for H
+        self.x = np.zeros((4, 1))# previous state vector, state mean know as x, u ==> for x
 
         # (x,y) tracking object center
-        self.b = np.array([[0], [255]])  # vector of observations
+        self.z = np.array([[1], [0]])# vector of observations, measurement matrix , b ==> for z
 
-        self.P = np.diag((3.0, 3.0))  # covariance matrix
-        self.F = np.array([[1.0, self.dt], [0.0, 1.0]])  # state transition mat
+        self.P = np.diag((3.0, 3.0, 3.0, 3.0)) * 1000# covariance matrix
 
-        self.Q = np.eye(self.u.shape[0])  # process noise matrix
-        self.R = np.eye(self.b.shape[0])  # observation noise matrix
+        self.F = np.array([[1, 0, self.dt, 0],
+                           [0, 1, 0, self.dt],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+        # state transition mat,     dinamics, transition matrix
+
+        self.Q = np.eye(self.x.shape[0])
+        # process noise matrixnp.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]) * 0.03
+        self.R = np.eye(self.z.shape[0])
+        # np.eye(self.z.shape[0])  # observation noise matrix np.array([[1,0,],[0,1]]) * 0.05
         self.lastResult = np.array([[0], [255]])
+
+        self.B = np.array([[0.5 * self.dt ** 2],
+                           [0.5 * self.dt ** 2],
+                           [self.dt],
+                           [self.dt]])
+        self.u = 50
 
     def predict(self):
         """Predict state vector u and variance of uncertainty P (covariance).
             where,
-            u: previous state vector
+            u: previous state vector, u ==> for x
             P: previous covariance matrix
             F: state transition matrix
             Q: process noise matrix
@@ -59,21 +62,23 @@ class KalmanFilter(object):
             vector of predicted state estimate
         """
         # Predicted state estimate
-        self.u = np.round(np.dot(self.F, self.u))
+        self.x = np.round(np.dot(self.F, self.x)) + np.round(np.dot(self.B, self.u))
         # Predicted estimate covariance
         self.P = np.dot(self.F, np.dot(self.P, self.F.T)) + self.Q
-        self.lastResult = self.u  # same last predicted result
-        return self.u
+        self.lastResult = self.x  # same last predicted result
+        # print ("pred", self.x)
+        return self.x
 
-    def correct(self, b, flag):
+    def correct(self, z, flag):
         """Correct or update state vector u and variance of uncertainty P (covariance).
         where,
-        u: predicted state vector u
-        A: matrix in observation equations
-        b: vector of observations
+        u: predicted state vector u ==> for x
+        A: matrix in observation equations, measurement function, A ==> for H
+        b: vector of observations, measurement matrix, b ==> for z
         P: predicted covariance matrix
         Q: process noise matrix
         R: observation noise matrix
+        C: system uncertainty , S ==> for C
         Equations:
             C = AP_{k|k-1} A.T + R
             K_{k} = P_{k|k-1} A.T(C.Inv)
@@ -90,22 +95,28 @@ class KalmanFilter(object):
         """
 
         if not flag:  # update using prediction
-            self.b = self.lastResult
-        else:  # update using detection
-            self.b = b
-        C = np.dot(self.A, np.dot(self.P, self.A.T)) + self.R
-        K = np.dot(self.P, np.dot(self.A.T, np.linalg.inv(C)))
+            self.z = np.array([[self.lastResult[0][0]], [self.lastResult[1][0]]])
+            LastR = self.z
 
-        self.u = np.round(self.u + np.dot(K, (self.b - np.dot(self.A,
-                                                              self.u))))
-        self.P = self.P - np.dot(K, np.dot(C, K.T))
-        self.lastResult = self.u
-        return self.u
+
+        else:  # update using detection
+            self.z = z
+
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        K = np.dot(self.P, np.dot(self.H.T, np.linalg.inv(S)))
+
+        self.x = np.round(self.x + np.dot(K, (self.z - np.dot(self.H, self.x))))
+        self.P = self.P - np.dot(K, np.dot(S, K.T))
+
+        if not flag:
+            self.x = self.lastResult
+
+        return self.x
 
 
 if __name__ == '__main__':
     SS = KalmanFilter()
-    z = [(10,10), (20,22), (33,31), (44,40), (55,55), (40,60)]
+    z = [(1,2), (2,2), (3,3), (4,2), (5,1)]
     for i in range(len(z)) :
         print('ordem',i)
         print('')
