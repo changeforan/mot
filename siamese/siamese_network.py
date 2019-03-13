8c092237d9700f151b3b6afcbf57191100cbbbde
deepin <changeforan@gmail.com>
2019-03-13 13:40:40 +0800

    dev

--- siamese/siamese_network.py
+++ siamese/siamese_network.py
@@ -7,14 +7,14 @@ MODEL_PATH = '/home/cs/work/football_match_mot/mot/save_models/siamese/model'
 class Siamese:
     def __init__(self):
         self.sess = tf.InteractiveSession()
-        self.model = inference.get_model()
+        self.model = inference.Siamese()
         saver = tf.train.Saver()
         saver.restore(self.sess, MODEL_PATH)
 
     def __del__(self):
         self.sess.close()
 
-    def run(self, *img):
+    def run(self, img):
         with self.sess.as_default():
-            o1 = self.model.o1.eval({self.model.x1: [i.reshape(-1) / 255 for i in img]})
+            o1 = self.model.o1.eval({self.model.x1: [img / 225]})
             return o1
