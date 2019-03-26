51774cb1d84a2be4d8efad458206acc673502d14
Sheng Chang <changeforan@gmail.com>
2019-03-26 20:00:18 +0800

    dev

--- helper/img_reader.py
+++ helper/img_reader.py
@@ -4,7 +4,7 @@ import cv2
 def open_path(path, start, end):
     total_img = os.listdir(path)[start - 1: end - 1]
     for img in total_img:
-        img_np = cv2.imread(path + img)
+        img_np = cv2.imread(os.path.join(path, img))
         yield img_np
 
 
