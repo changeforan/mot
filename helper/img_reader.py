e9a526d7e788cb0d0fc84ac9c60eee8bf5c32242
Sheng Chang <changeforan@gmail.com>
2019-03-26 20:51:57 +0800

    dev

--- helper/img_reader.py
+++ helper/img_reader.py
@@ -2,7 +2,7 @@ import os
 import cv2
 
 def open_path(path, start, end):
-    total_img = os.listdir(path)[start - 1: end - 1]
+    total_img = os.listdir(path).sort()[start - 1: end - 1]
     for img in total_img:
         img_np = cv2.imread(os.path.join(path, img))
         print(img)
