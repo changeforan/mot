58e0ea666d93b7756d807344c1ccd75f6e28f087
Sheng Chang <changeforan@gmail.com>
2019-03-27 14:57:21 +0800

    dev

--- helper/bbox_tools.py
+++ helper/bbox_tools.py
@@ -39,6 +39,6 @@ def bbox_iou(bbox_a, bbox_b):
     area_i = xp.prod(br - tl, axis=2) * (tl < br).all(axis=2)
     area_a = xp.prod(bbox_a[:, 2:] - bbox_a[:, :2], axis=1)
     area_b = xp.prod(bbox_b[:, 2:] - bbox_b[:, :2], axis=1)
-    return area_i / (area_a[:, None])
+    return area_i / (area_a[:, None] + area_b - area_i)
 
 
