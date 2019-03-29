70f677541809b5b94484e6ed71674113c6140a30
Sheng Chang <changeforan@gmail.com>
2019-03-29 21:09:56 +0800

    dev

--- helper/video_util.py
+++ helper/video_util.py
@@ -12,10 +12,21 @@ def open_video(path, max_frame):
     cv2.destroyAllWindows()
 
 
-def save_video(path, image_np_list):
+def save_img(path, image_np_list):
     index = 0
     for image_np in image_np_list:
-
         name = str(index) + '.jpg'
         index += 1
         cv2.imwrite(os.path.join(path, name), image_np)
+
+
+def save_video(path, image_np_list):
+    name = path + '.avi'
+    out = cv2.VideoWriter(
+            name,
+            cv2.VideoWriter_fourcc(*'XVID'),
+            20.0,
+            (800, 600))
+    for image_np in image_np_list:
+        out.write(cv2.resize(image_np, (800, 600)))
+    cv2.destroyAllWindows()
